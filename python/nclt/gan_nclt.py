90047cd160ed4fff523fe44595d8d99abc118274
Arash Ushani <aushani@gmail.com>
2017-05-17 16:40:58 -0400

    why not try gan too

--- python/nclt/gan_nclt.py
+++ python/nclt/gan_nclt.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 from data_manager import *
 
-class ALI:
+class GAN:
 
   def __init__(self, sess, data_dim, z_dim):
 
@@ -25,22 +25,16 @@ class ALI:
     self.z = tf.placeholder(tf.float32, [None, self.z_dim], name = 'z')
 
     # Generator
-    self.encoder = self.make_encoder(self.real_data)
     self.decoder = self.make_decoder(self.z)
 
     # Discriminator
-    self.discriminator_real = self.make_discriminator(self.real_data, self.encoder, reuse=False)
-    self.discriminator_syth = self.make_discriminator(self.decoder, self.z, reuse=True)
+    self.discriminator_real = self.make_discriminator(self.real_data, reuse=False)
+    self.discriminator_syth = self.make_discriminator(self.decoder, reuse=True)
 
     # Loss Functions
-    self.encoder_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
-                            logits=self.discriminator_real, labels=tf.ones([self.batch_size], tf.int32)),
-                            name='encoder_loss')
-    self.decoder_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
+    self.generator_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
                             logits=self.discriminator_syth, labels=tf.zeros([self.batch_size], tf.int32)),
-                            name='decoder_loss')
-
-    self.generator_loss = tf.add(self.encoder_loss, self.decoder_loss, name='generator_loss')
+                            name='generator_loss')
 
     self.discriminator_loss_real = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
                                    logits=self.discriminator_real, labels=tf.zeros([self.batch_size], tf.int32)))
@@ -52,59 +46,14 @@ class ALI:
 
     # Evalualation
     self.query_data = tf.placeholder(tf.float32, [None] +  self.data_dim, name='query_data')
-    self.query_z = tf.placeholder(tf.float32, [None, self.z_dim], name = 'query_z')
-    self.query_logits = self.make_discriminator(self.query_data, self.query_z, reuse=True)
+    self.query_logits = self.make_discriminator(self.query_data, reuse=True)
     self.query_softmax = tf.nn.softmax(self.query_logits)
 
     # Summaries
-    self.encoder_loss_summary = tf.summary.scalar('encoder_loss_summary', self.encoder_loss)
-    self.decoder_loss_summary = tf.summary.scalar('decoder_loss_summary', self.decoder_loss)
     self.generator_loss_summary = tf.summary.scalar('generator_loss_summary', self.generator_loss)
     self.discriminator_real_loss_summary = tf.summary.scalar('discriminator_real_loss_summary', self.discriminator_loss_real)
     self.discriminator_syth_loss_summary = tf.summary.scalar('discriminator_syth_loss_summary', self.discriminator_loss_syth)
     self.discriminator_loss_summary = tf.summary.scalar('discriminator_loss_summary', self.discriminator_loss)
-
-  def make_encoder(self, x):
-
-    stage_layers = [16, 32, 64, 128, 256]
-
-    prev_res = x
-
-    with tf.variable_scope('Encoder'):
-      for i in range(len(stage_layers)):
-        H_conv = tf.layers.conv3d(inputs=prev_res, filters=stage_layers[i], kernel_size=[4, 4, 4],
-                                    padding="same", activation=tf.nn.relu)
-
-        H_mp = tf.layers.max_pooling3d(inputs=H_conv, pool_size=[2, 2, 2], strides=2)
-        if self.use_batch_normalization:
-          H_bn = tf.layers.batch_normalization(inputs=H_mp)
-          H_i = H_bn
-        else:
-          H_i = H_mp
-
-        print 'Encoder conv layer %d:' % (i), H_i.get_shape().as_list()
-        prev_res = H_i
-
-      # Now add dense layer
-      last_shape = prev_res.get_shape().as_list()
-      n = last_shape[1]*last_shape[2]*last_shape[3]*last_shape[4]
-      flat = tf.reshape(prev_res, shape=[-1, n])
-      fc = tf.layers.dense(inputs=flat, units=2*self.z_dim)
-
-      if self.use_batch_normalization:
-        fc_bn = tf.layers.batch_normalization(inputs=fc)
-        fc = fc_bn
-
-      fc = tf.nn.relu(fc)
-
-      print 'Encoder fully connected layer:', fc.get_shape().as_list()
-
-      # Output layer
-      output = tf.layers.dense(inputs=fc, units=self.z_dim)
-      print 'Encoder output layer:', output.get_shape().as_list()
-
-      return output
-
 
   def make_decoder(self, z):
     stage_layers = [256, 128, 64, 32, 16]
@@ -166,20 +115,15 @@ class ALI:
 
       return H_out
 
-  def make_discriminator(self, x, z, reuse=False):
+  def make_discriminator(self, x, reuse=False):
 
     stage2_layers = [64, 32, 16]
 
     with tf.variable_scope('Discriminator', reuse=reuse):
 
-      # Append x and z for input layer
       n = self.data_dim[0]*self.data_dim[1]*self.data_dim[2]*self.data_dim[3]
       x_flat = tf.reshape(x, shape=[-1, n])
-      cc = tf.concat([x_flat, z], axis=1)
-
-      print 'Discriminator cc layer:', cc.get_shape().as_list()
-
-      prev_res = cc
+      prev_res = x_flat
 
       for i in range(len(stage2_layers)):
         H_fc = tf.layers.dense(inputs=prev_res, units=stage2_layers[i], activation=tf.nn.relu)
@@ -205,7 +149,7 @@ class ALI:
   def train(self):
     # Get vars
     t_vars = tf.trainable_variables()
-    self.g_vars = [var for var in t_vars if 'Encoder' in var.name or 'Decoder' in var.name]
+    self.g_vars = [var for var in t_vars if 'Decoder' in var.name]
     self.d_vars = [var for var in t_vars if 'Discriminator' in var.name]
 
     learning_rate = 1e-3
@@ -230,7 +174,7 @@ class ALI:
     sample_z = np.random.normal(0, 1, [sample_size, self.z_dim])
 
     d_sums = tf.summary.merge([self.discriminator_loss_summary, self.discriminator_real_loss_summary, self.discriminator_syth_loss_summary])
-    g_sums = tf.summary.merge([self.generator_loss_summary, self.decoder_loss_summary, self.encoder_loss_summary])
+    g_sums = tf.summary.merge([self.generator_loss_summary])
 
     while True:
       # Get data
@@ -268,15 +212,6 @@ class ALI:
 
         save_path = saver.save(sess, "./model.ckpt")
 
-        # Query
-        #query_data = self.dm.get_next_batch()
-        #query_z = self.encoder.eval({self.real_data: query_data})
-        #print 'real', self.query_softmax.eval({self.query_data: query_data, self.query_z: query_z})
-
-        #query_z = sample_z
-        #query_data = self.decoder.eval({self.z: query_z})
-        #print 'syth', self.query_softmax.eval({self.query_data: query_data, self.query_z: query_z})
-
         # Generate eval
         fd = {self.z: sample_z}
         sample_grids = self.decoder.eval(fd)
@@ -293,5 +228,5 @@ class ALI:
 
 gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.3)
 with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
-  ali = ALI(sess, [32, 32, 32, 1], 256)
-  ali.train()
+  gan = GAN(sess, [32, 32, 32, 1], 256)
+  gan.train()
