bd9a6e8fdbf9fd22b8ce3a806f388eab6b58f286
Arash Ushani <aushani@gmail.com>
2017-06-08 12:31:21 -0400

    direct shape completion

--- python/shape_completion/data_manager.py
+++ python/shape_completion/data_manager.py
@@ -1,5 +1,5 @@
 from os import listdir
-from os.path import isfile, isdir, join
+from os.path import isfile, isdir, join, basename, splitext
 
 import random
 import numpy as np
@@ -9,22 +9,37 @@ import threading
 import time
 import Queue
 
+import bisect
+import re
+
 class DataManager:
 
-  def __init__(self, dirname, batch_size = 1, dim=[32, 32, 32, 2]):
-    self.dirname = dirname
-    self.dim = dim
-
+  def __init__(self, dirname_df, dirname_sdf, batch_size=1):
     self.batch_size = batch_size
 
-    dirs = [f for f in listdir(dirname) if isdir(join(dirname, f))]
+    dirs_df = [f for f in listdir(dirname_df) if isdir(join(dirname_df, f))]
+    files_df = [join(d, f) for d in dirs_df for f in listdir(join(dirname_df, d)) if '.df' in f]
+    files_df.sort()
 
-    files = [dirname + '/' + d + '/' + f for d in dirs for f in listdir(dirname + '/' + d) if '.df' in f]
-    self.files = files
+    dirs_sdf = [f for f in listdir(dirname_sdf) if isdir(join(dirname_sdf, f))]
+    files_sdf = [join(d, f) for d in dirs_sdf for f in listdir(join(dirname_sdf, d)) if '.sdf' in f]
+    files_sdf.sort()
 
-    random.shuffle(self.files)
+    # Find pairs that exist in both
+    self.file_pairs = {}
+    for sdf in files_sdf:
+      root, ext = splitext(sdf)
+      df_cand = re.sub('__\d+__.sdf', '__0__.df', sdf)
+      i = bisect.bisect_left(files_df, df_cand)
+      if i != len(files_df) and files_df[i] == df_cand:
+        self.file_pairs[join(dirname_sdf, sdf)] = join(dirname_df, df_cand)
+
+    print 'Have %d samples' % (len(self.file_pairs))
+    self.files_sdf = self.file_pairs.keys()
+    random.shuffle(self.files_sdf)
 
     self.idx_at = 0
+    self.epoch = 1
 
     # Start load threads
     self.data_queue = Queue.Queue(maxsize=1024)
@@ -40,16 +55,15 @@ class DataManager:
 
   def load_next(self):
     res = self.load(self.idx_at)
-    self.idx_at = (self.idx_at + 1) % len(self.files)
+    self.idx_at += 1
+    if self.idx_at >= len(self.file_pairs):
+      self.idx_at = 0
+      print 'Starting epoch %04d...' % (self.epoch)
 
     return res
 
-  def load(self, idx):
-    path = self.files[self.idx_at]
-
-    fp = open(path, 'r')
-
-    # We have a dense array of voxels
+  def read_file(self, fn):
+    fp = open(fn, 'r')
     dim_str = fp.read(3*8)
     num_voxels = 32*32*32
     grid_bin = struct.unpack('f'*num_voxels, fp.read(4*num_voxels))
@@ -59,28 +73,44 @@ class DataManager:
 
     # Build grid
     grid = np.asarray(grid_bin)
-    grid = np.reshape(grid, [32, 32, 32], order='C')
+    grid = np.reshape(grid, [1, 32, 32, 32], order='C')
 
-    sample = np.zeros(self.dim)
-    sample[:, :, :, 0] = (np.isfinite(grid) & (np.abs(grid) < 0.5))
-    #sample[:, :, :, 0] = (np.isfinite(grid) & (np.abs(grid) < 0.5)) * 2 - 1
-    sample[:, :, :, 1] = self.scale_df(grid)
+    return grid
 
-    return sample
+  def load(self, idx):
+    sdf = self.files_sdf[self.idx_at]
+    df = self.file_pairs[sdf]
 
-  def scale_df(self, df):
+    grid_sdf = self.read_file(sdf)
+    grid_df = self.read_file(df)
+
+    sample_sdf = np.empty([1, 32, 32, 32, 2])
+    sample_df = np.empty([1, 32, 32, 32, 1])
+
+    sample_sdf[:, :, :, :, 0] = self.scale(grid_sdf)
+    sample_sdf[:, :, :, :, 1] = np.sign(grid_sdf)
+
+    sample_df[:, :, :, :, 0] = self.scale(grid_df)
+
+    return (sample_sdf, sample_df)
+
+  def scale(self, df):
+      df[np.isinf(df)] = np.max(df[np.isfinite(df)])
       return np.log(np.abs(df)+1.0)
 
-  def unscale_df(self, df):
+  def unscale(self, df):
       return np.exp(df)-1.0
 
   def load_files(self):
     while True:
-      batch = np.empty([self.batch_size] + self.dim)
+      samples_sdf = np.empty([self.batch_size, 32, 32, 32, 2])
+      samples_df = np.empty([self.batch_size, 32, 32, 32, 1])
       for i in range(self.batch_size):
-        batch[i, :, :, :, :] = self.load_next()
+        sdf, df = self.load_next()
+        samples_sdf[i, :, :, :, :] = sdf
+        samples_df[i, :, :, :, :] = df
 
-      self.data_queue.put(batch)
+      self.data_queue.put((samples_sdf, samples_df))
 
   def get_next_batch(self):
     item = self.data_queue.get()
@@ -91,69 +121,30 @@ class DataManager:
   def queue_size(self):
     return self.data_queue.qsize()
 
-  def save(self, sample, path):
-    locs = []
-    lls = []
+  def save(self, grid, path):
+    f = open(path, 'w')
 
-    df = self.unscale_df(sample[:, :, :, 1])
+    f.write(struct.pack('q', 32))
+    f.write(struct.pack('q', 32))
+    f.write(struct.pack('q', 32))
 
-    for i in range(self.dim[0]):
-      for j in range(self.dim[1]):
-        for k in range(self.dim[2]):
-          locs.append(i-self.dim[0]/2)
-          locs.append(j-self.dim[1]/2)
-          locs.append(k-self.dim[2]/2)
+    df = self.unscale(grid)
 
-          if np.isfinite(df[i, j, k]) and abs(df[i, j, k]) < 0.5:
-          #if sample[i, j, k, 0] > 0:
-            p = 0.99
-          else:
-            p = 0.01
+    for i in range(32):
+      for j in range(32):
+        for k in range(32):
+          f.write(struct.pack('f', df[i, j, k]))
 
-          ll = np.log(p/(1-p))
-
-          lls.append(ll)
-
-    fp = open(path, 'w')
-
-    resolution = 1.0
-    fp.write(struct.pack('f', resolution))
-
-    num_voxels = len(lls)
-    fp.write(struct.pack('L', num_voxels))
-
-    fp.write(struct.pack('iii'*num_voxels, *locs))
-    fp.write(struct.pack('f'*num_voxels, *lls))
-
-    fp.close()
-
-  def save_dense(self, sample, path):
-
-    df = self.unscale_df(sample[:, :, :, 1])
-
-    fp = open(path, 'w')
-
-    for i in range(self.dim[0]):
-      for j in range(self.dim[1]):
-        for k in range(self.dim[2]):
-          if np.abs(df[i, j, k]) < 0.5:
-            val = 0.99
-          else:
-            val = 0.01
-
-          fp.write(struct.pack('f', val))
-
-    fp.close()
+    f.close()
 
 if __name__ == '__main__':
   import matplotlib.pyplot as plt
   from mpl_toolkits.mplot3d import Axes3D
 
-  dm = DataManager('/home/aushani/data/shapenet_dim32_df/')
-
-  res = dm.get_next_batch()
-
-  print res.shape
-  sample = res[0, :, :, :, :]
-  print sample.shape
-  dm.save_dense(sample, 'test.og')
+  dm = DataManager('/home/aushani/data/shapenet_dim32_df', '/home/aushani/data/shapenet_dim32_sdf')
+  x = dm.get_next_batch()[0]
+  print x.shape
+  x = x[0, :, :, :, 1]
+  print x.shape
+  print np.sum(x<0)
+  print np.sum(x>0)
--- python/shape_completion/direct.py
+++ python/shape_completion/direct.py
@@ -6,13 +6,30 @@ from data_manager import *
 
 class Direct:
 
-    def __init__(self, sess):
+    def __init__(self, sess, batch_size=1):
 
-        self.batch_size = 1
+        self.batch_size = batch_size
 
-        self.df = tf.placeholder(tf.float32, [self.batch_size, 32, 32, 32, 2], name='distance_field')
+        self.dm = DataManager('/home/aushani/data/shapenet_dim32_df', '/home/aushani/data/shapenet_dim32_sdf', batch_size = batch_size)
 
-        self.make_epn(self.df)
+        self.sess = sess
+
+        self.sdf = tf.placeholder(tf.float32, [self.batch_size, 32, 32, 32, 2], name='sdf')
+        self.df = tf.placeholder(tf.float32, [self.batch_size, 32, 32, 32, 1], name='df')
+
+        self.epn = self.make_epn(self.sdf)
+
+        # Syntheic shape (only fill in unknown)
+        sdf_known = tf.slice(self.sdf, [0, 0, 0, 0, 0], [self.batch_size, 32, 32, 32, 1])
+        sdf_sign = tf.slice(self.sdf, [0, 0, 0, 0, 1], [self.batch_size, 32, 32, 32, 1])
+        self.syn = tf.where(tf.greater_equal(sdf_sign, 0), sdf_known, self.epn)
+
+        # Loss
+        abs_err = tf.abs(tf.subtract(self.syn, self.df))
+        self.loss = tf.reduce_sum(abs_err)/(self.batch_size*32*32*32)
+
+        # Summaries
+        self.loss_summary = tf.summary.scalar('loss_summary', self.loss)
 
     def make_epn(self, data):
 
@@ -21,14 +38,15 @@ class Direct:
 
         # encoder network
         last = data
+        enc = [None,]*layers
         for i in range(layers):
-            enc = tf.layers.conv3d(inputs=last, filters=(2**i)*nf, kernel_size=[4, 4, 4],
-                    padding="same", activation=tf.nn.relu)
-            enc = tf.layers.max_pooling3d(inputs=enc, pool_size=[2, 2, 2], strides=2)
-            enc = tf.layers.batch_normalization(enc)
-            print 'Enc %d :' % (i), enc.get_shape().as_list()
+            enc[i] = tf.layers.conv3d(inputs=last, filters=(2**i)*nf, kernel_size=[4, 4, 4],
+                       padding="same", activation=tf.nn.relu)
+            enc[i] = tf.layers.max_pooling3d(inputs=enc[i], pool_size=[2, 2, 2], strides=2)
+            enc[i] = tf.layers.batch_normalization(enc[i])
+            print 'Enc %d :' % (i), enc[i].get_shape().as_list()
 
-            last = enc
+            last = enc[i]
 
         # dense layers
         enc_reshaped = tf.reshape(last, shape=[self.batch_size, -1])
@@ -42,17 +60,123 @@ class Direct:
         print 'reshaped :', last.get_shape().as_list()
 
         for i in range(layers):
-            W = tf.get_variable('W_%d' % (i), [4, 4, 4, (2**(layers-i-2))*nf, (2**(layers-i-1))*nf])
-            b = tf.get_variable('b_%d' % (i), [(2**(layers-i-2))*nf])
+            # skip connection
+            last = tf.concat([last, enc[-i-1]], 4)
+            print ' With skip :', last.get_shape().as_list()
+
+            output_size = int(2**(i+1))
+            this_layers = last.get_shape().as_list()[-1]
+            next_layers = (2**(layers-i-2))*nf
+            if i is layers-1:
+                next_layers = 1
+            W = tf.get_variable('W_%d' % (i), [4, 4, 4, next_layers, this_layers])
+            b = tf.get_variable('b_%d' % (i), [next_layers])
             dec = tf.nn.conv3d_transpose(last, W,
-                    [self.batch_size, 2, 2, 2, (2**layers-i-1)*nf], strides=[1, 1, 1, 1, 1],
+                    [self.batch_size, output_size, output_size, output_size, next_layers], strides=[1, 2, 2, 2, 1],
                     padding="SAME")
             dec = tf.nn.bias_add(dec, b)
-            dec = tf.nn.relu(dec)
+            if i < layers-1:
+                dec = tf.nn.relu(dec)
+                dec = tf.layers.batch_normalization(dec)
             print 'Dec %d: ' % (i), dec.get_shape().as_list()
 
             last = dec
 
-gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.3)
+        res = tf.log(1 + tf.abs(last))
+        print 'Res: ', res.get_shape().as_list()
+
+        return res
+
+    def train(self):
+        # Get vars
+        t_vars = tf.trainable_variables()
+
+        learning_rate = 0.001
+        beta1 = 0.9
+        optim = tf.train.AdamOptimizer(learning_rate = learning_rate, beta1 = beta1).minimize(self.loss, var_list = t_vars)
+        #optim = tf.train.AdamOptimizer().minimize(self.loss)
+        #optim = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(self.loss, var_list=t_vars)
+        tf.global_variables_initializer().run()
+
+        self.writer = tf.summary.FileWriter('./logs', self.sess.graph)
+        saver = tf.train.Saver()
+
+        step = 0
+
+        load_time = 0
+        train_time = 0
+        step_last = step
+        stats_step_min = 15
+        stats_count = 0
+
+        sums = tf.summary.merge([self.loss_summary])
+
+        tic_last = 0
+
+        while True:
+            # Get data
+            tic_load = time.time()
+            data = self.dm.get_next_batch()
+            fd = {self.sdf: data[0], self.df: data[1]}
+            toc_load = time.time()
+            load_time += toc_load - tic_load
+
+            tic_train = time.time()
+            _, summary = self.sess.run([optim, sums], feed_dict = fd)
+            self.writer.add_summary(summary, step)
+            toc_train = time.time()
+            train_time += toc_train - tic_train
+
+            step += 1
+
+            if (time.time() - tic_last) > stats_step_min*60:
+                print '------------------------------'
+                print '  Training Step %06d' % (step)
+                print ''
+                print '  Waiting %5.3f sec / step for loading (%d queued)' % (load_time / (step - step_last), self.dm.queue_size())
+                print '  Waiting %5.3f sec / step for training' % (train_time / (step - step_last))
+
+                load_time = 0
+                train_time = 0
+                tic_last = time.time()
+                step_last = step
+                stats_count += 1
+
+                save_path = saver.save(sess, "./model.ckpt")
+
+                # Try making a dummy result
+                sdfs = data[0]
+                dfs = data[1]
+                dfs_raw = self.epn.eval(feed_dict = {self.sdf: sdfs})
+                dfs_gen = self.syn.eval(feed_dict = {self.sdf: sdfs})
+
+                # verify
+                tic_verify = time.time()
+                count_known = 0
+                count_unknown = 0
+                for batch_i in range(self.batch_size):
+                    for i in range(32):
+                      for j in range(32):
+                        for k in range(32):
+                          if sdfs[batch_i, i, j, k, 1] >= 0:
+                            if np.abs(sdfs[batch_i, i, j, k, 0] - dfs_gen[batch_i, i, j, k, 0])>1e-2:
+                              count_known += 1
+                          else:
+                            if np.abs(dfs_raw[batch_i, i, j, k, 0] - dfs_gen[batch_i, i, j, k, 0])>1e-2:
+                              count_unknown += 1
+                toc_verify = time.time()
+                print '  Verification: %d known mismatches, %d unknown mismatches' % (count_known, count_unknown)
+                print '  Verification took %5.3f sec' % (toc_verify - tic_verify)
+
+                for batch_i in range(self.batch_size):
+                    self.dm.save(sdfs[batch_i, :, :, :, 0],     'results/shape_%02d_%05d.sdf'   % (batch_i, stats_count))
+                    self.dm.save(dfs[batch_i, :, :, :, 0],      'results/shape_%02d_%05d.df'    % (batch_i, stats_count))
+                    self.dm.save(dfs_raw[batch_i, :, :, :, 0],  'results/shape_%02d_%05d.dfraw' % (batch_i, stats_count))
+                    self.dm.save(dfs_gen[batch_i, :, :, :, 0],  'results/shape_%02d_%05d.dfgen' % (batch_i, stats_count))
+
+batch_size = 16
+
+gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.9)
 with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
-  direct = Direct(sess)
+    direct = Direct(sess, batch_size = batch_size)
+    direct.train()
