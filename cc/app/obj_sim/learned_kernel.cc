ed9c19e81ec3ffbaf8a1adefa5c8e339f57e4f17
Arash Ushani <aushani@gmail.com>
2017-06-26 08:48:27 -0400

    adding learned kernel

---
+++ cc/app/obj_sim/learned_kernel.cc
@@ -0,0 +1,57 @@
+#include "app/obj_sim/learned_kernel.h"
+
+#include <cmath>
+#include <iostream>
+
+LearnedKernel::LearnedKernel(float size, float res) :
+  dim_size_(ceil(size/res)),
+  res_(res),
+  vals_(dim_size_*dim_size_, 0.0f) {
+}
+
+void LearnedKernel::CopyFrom(const hm::IKernel &kernel) {
+  for (int i=0; i<dim_size_; i++) {
+    for (int j=0; j<dim_size_; j++) {
+      float dx = i*res_;
+      float dy = j*res_;
+
+      SetPixel(i, j, kernel.Evaluate(dx, dy));
+    }
+  }
+}
+
+float LearnedKernel::Evaluate(float dx, float dy) const {
+  int i = round(std::fabs(dx)/res_);
+  int j = round(std::fabs(dy)/res_);
+
+  return GetPixel(i, j);
+}
+
+float LearnedKernel::MaxSupport() const {
+  return res_*dim_size_;
+}
+
+hm::DeviceKernelTable LearnedKernel::MakeDeviceKernelTable() const {
+  hm::DeviceKernelTable kt(GetDimSize(), MaxSupport());
+  kt.SetData(vals_.data());
+  return kt;
+}
+
+float LearnedKernel::GetPixel(int i, int j) const {
+  if (i<0 || i>=dim_size_ || j<0 || j>=dim_size_)
+    return 0.0;
+
+  size_t idx = i*dim_size_ + j;
+
+  return vals_[idx];
+}
+
+void LearnedKernel::SetPixel(int i, int j, float x) {
+  size_t idx = i*dim_size_ + j;
+
+  vals_[idx] = x;
+}
+
+size_t LearnedKernel::GetDimSize() const {
+  return dim_size_;
+}
---
+++ cc/app/obj_sim/learned_kernel.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <vector>
+#include <cstdlib>
+
+#include "library/hilbert_map/kernel.h"
+
+namespace hm = library::hilbert_map;
+
+class LearnedKernel : public hm::IKernel {
+ public:
+  LearnedKernel(float size, float res);
+
+  void CopyFrom(const hm::IKernel &kernel);
+
+  float Evaluate(float dx, float dy) const override;
+  float MaxSupport() const override;
+
+  hm::DeviceKernelTable MakeDeviceKernelTable() const override;
+
+  float GetPixel(int i, int j) const;
+  void SetPixel(int i, int j, float x);
+
+  size_t GetDimSize() const;
+
+ private:
+  size_t dim_size_;
+  float res_;
+
+  std::vector<float> vals_;
+
+};
