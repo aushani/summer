b0b226442131321f8b35d08c51a3e6130b175d5a
Arash Ushani <aushani@gmail.com>
2017-08-11 09:20:25 -0400

    histogram -> map instead of vector

--- cc/library/histogram/histogram.cc
+++ cc/library/histogram/histogram.cc
@@ -13,8 +13,8 @@ Histogram::Histogram() {
 
 Histogram::Histogram(double min, double max, double res) :
  min_(min), max_(max), res_(res), counts_total_(0.0f) {
-  int dim = ceil((max - min)/res);
-  counts_ = std::vector<double>(dim + 3, 0.0f); // Add 2 so we keep track of values out of range as well, plus 1 for rounding
+  size_t dim = ceil((max - min)/res);
+  counts_index_max_ = dim + 2;
 }
 
 bool Histogram::InRange(double val) const {
@@ -34,11 +34,13 @@ double Histogram::GetRes() const {
 }
 
 void Histogram::Mark(double val, double weight) {
-  if (counts_total_ == 0.0 || val < observed_min_)
+  if (counts_total_ == 0.0 || val < observed_min_) {
     observed_min_ = val;
+  }
 
-  if (counts_total_ == 0.0 || val > observed_max_)
+  if (counts_total_ == 0.0 || val > observed_max_) {
     observed_max_ = val;
+  }
 
   size_t idx = GetIndex(val);
   counts_[idx] += weight;
@@ -46,15 +48,20 @@ void Histogram::Mark(double val, double weight) {
 }
 
 void Histogram::Clear() {
-  for (size_t i = 0; i<counts_.size(); i++) {
-    counts_[i] = 0.0f;
-  }
+  counts_.clear();
   counts_total_ = 0.0f;
 }
 
 double Histogram::GetCount(double val) const {
   size_t idx = GetIndex(val);
-  return counts_[idx];
+
+  // Be careful not to increase size of map if we don't have to
+  const auto it = counts_.find(idx);
+  if (it == counts_.end()) {
+    return 0;
+  }
+
+  return it->second;
 }
 
 double Histogram::GetCountsTotal() const {
@@ -67,36 +74,13 @@ double Histogram::Sample() const {
   std::default_random_engine rand_engine(seed);
 
   double cdf = rand_cdf(rand_engine);
-
-  double cdf_at = 0;
-  size_t idx_at = 0;
-
-  double v1 = observed_min_;
-  double cdf_left = 0;
-
-  while (cdf_at < cdf && idx_at < counts_.size()) {
-    v1 = GetValue(idx_at);
-    cdf_left = (cdf - cdf_at);
-
-    idx_at++;
-    cdf_at += counts_[idx_at]/counts_total_;
-  }
-
-  // Linearly interpolate?
-  double v2 = observed_max_;
-  if (idx_at < counts_.size())
-    v2 = GetValue(idx_at);
-
-  double p = counts_[idx_at]/counts_total_;
-
-  double res = v1 + (v2-v1)*(cdf_left/p);
-
-  return res;
+  return GetPercentile(cdf);
 }
 
 double Histogram::GetProbability(double val) const {
-  if (counts_total_ == 0.0f)
+  if (counts_total_ == 0.0f) {
     return 0.0f;
+  }
 
   double count = GetCount(val);
   return count/counts_total_;
@@ -124,9 +108,14 @@ double Histogram::GetLikelihood(double val) const {
 
 double Histogram::GetCumulativeProbability(double val) const {
   int count = 0;
-  for (size_t i=0; i<=GetIndex(val); i++) {
-    count += counts_[i];
+  for (auto it = counts_.cbegin(); it != counts_.cend(); it++) {
+    if (GetValue(it->first) > val) {
+      break;
+    }
+
+    count += it->second;
   }
+
   return count/counts_total_;
 }
 
@@ -142,19 +131,19 @@ double Histogram::GetPercentile(double percentile) const {
   double count_at = 0;
   double target_count = percentile * counts_total_;
 
-  for (size_t i=0; i < counts_.size(); i++) {
-    double count_i = counts_[i];
+  for (auto it = counts_.cbegin(); it != counts_.cend(); it++) {
+    double count_it = it->second;
     double count_left = target_count - count_at;
 
-    if (count_i < count_left) {
-      count_at += count_i;
+    if (count_it < count_left) {
+      count_at += count_it;
       continue;
     }
 
-    double frac = count_left/count_i;
+    double frac = count_left/count_it;
 
-    double v1 = GetValue(i);
-    double v2 = GetValue(i+1);
+    double v1 = GetValue(it->first);
+    double v2 = GetValue(it->first+1);
 
     return v1 + (v2 - v1)*frac;
   }
@@ -167,19 +156,25 @@ double Histogram::GetMedian() const {
 }
 
 size_t Histogram::GetIndex(double val) const {
-  if (val <= min_)
+  if (val <= min_) {
     return 0;
-  if (val >= max_)
-    return counts_.size() - 1;
+  }
+
+  if (val >= max_) {
+    return counts_index_max_;
+  }
 
   return floor((val - min_) / res_) + 1;
 }
 
 double Histogram::GetValue(size_t idx) const {
-  if (idx <= 0)
+  if (idx <= 0) {
     return min_;
-  if (idx >= counts_.size()-1)
+  }
+
+  if (idx >= counts_index_max_) {
     return max_;
+  }
 
   return (idx-1)*res_ + min_;
 }
