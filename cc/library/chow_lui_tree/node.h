3b648232cc1d2a9d9a32b72da532ecbfeb6b96d1
Arash Ushani <aushani@gmail.com>
2017-08-30 18:03:25 -0400

    wip

---
+++ cc/library/chow_lui_tree/node.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "library/ray_tracing/occ_grid_location.h"
+
+#include "library/chow_lui_tree/joint_model.h"
+
+namespace rt = library::ray_tracing;
+
+namespace library {
+namespace chow_lui_tree {
+
+class Node {
+ public:
+  Node(const rt::Location &my_loc, const JointModel &jm);
+  Node(const rt::Location &my_loc, const Node &parent, const JointModel &jm);
+
+ private:
+  struct MarginalDistribution {
+
+  };
+
+  struct ConditionalDistribution {
+
+  };
+
+  rt::Location location_;
+
+  boost::optional<std::shared_ptr<Node>> parent_;
+
+};
+
+}
+}
