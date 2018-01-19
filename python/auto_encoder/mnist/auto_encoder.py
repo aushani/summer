a03345c8f11349e9a723a559b93dd7b2cb0ed279
Arash Ushani <aushani@gmail.com>
2018-01-18 19:07:30 -0500

    wip better mnist exp

--- python/auto_encoder/mnist/auto_encoder.py
+++ python/auto_encoder/mnist/auto_encoder.py
@@ -5,7 +5,7 @@ import numpy as np
 
 class AutoEncoder:
 
-    def __init__(self, use_classification_loss = False):
+    def __init__(self):
         dim_data = 784
         n_classes = 10
 
@@ -17,48 +17,68 @@ class AutoEncoder:
 
         # Encoder
         l1 = tf.contrib.layers.fully_connected(self.input, 50,
-                activation_fn=tf.nn.tanh, weights_regularizer=regularizer)
+                activation_fn=tf.nn.tanh, weights_regularizer=regularizer, scope='autoencoder/l1')
 
         l2 = tf.contrib.layers.fully_connected(l1, 50,
-                activation_fn=tf.nn.tanh, weights_regularizer=regularizer)
+                activation_fn=tf.nn.tanh, weights_regularizer=regularizer, scope='autoencoder/l2')
 
         self.latent = tf.contrib.layers.fully_connected(l2, 2,
-                activation_fn=None, weights_regularizer=regularizer)
+                activation_fn=None, weights_regularizer=regularizer, scope='autoencoder/latent')
 
         # Decoder
         l3 = tf.contrib.layers.fully_connected(self.latent, 50,
-                activation_fn=tf.nn.tanh, weights_regularizer=regularizer)
+                activation_fn=tf.nn.tanh, weights_regularizer=regularizer, scope='autoencoder/l3')
 
         l4 = tf.contrib.layers.fully_connected(l3, 50,
-                activation_fn=tf.nn.tanh, weights_regularizer=regularizer)
+                activation_fn=tf.nn.tanh, weights_regularizer=regularizer, scope='autoencoder/l4')
 
         self.reconstruction = tf.contrib.layers.fully_connected(l4, dim_data,
-                activation_fn=tf.nn.sigmoid, weights_regularizer=regularizer)
+                activation_fn=tf.nn.sigmoid, weights_regularizer=regularizer, scope='autoencoder/rec')
 
         # Classifier
         self.pred_label = tf.contrib.layers.fully_connected(self.latent, n_classes,
-                activation_fn=None, weights_regularizer=regularizer)
+                activation_fn=None, weights_regularizer=regularizer, scope='classifier')
 
-        classification_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.label, logits=self.pred_label))
-        reconstruction_loss = tf.reduce_mean(tf.squared_difference(self.reconstruction, self.input))
+        # Losses
+        self.classification_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.label, logits=self.pred_label))
+        self.classification_loss += tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES, scope='classifier')
 
-        if use_classification_loss:
-            self.loss = classification_loss + reconstruction_loss
-        else:
-            self.loss = reconstruction_loss
-
-        reg_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
-        self.loss += reg_losses
-
-        #train_step = tf.train.GradientDescentOptimizer(0.01).minimize(self.loss)
-        self.train_step = tf.train.AdamOptimizer(1e-4).minimize(self.loss)
+        self.reconstruction_loss = tf.reduce_mean(tf.squared_difference(self.reconstruction, self.input))
+        self.reconstruction_loss += tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES, scope='autoencoder')
 
         self.sess = tf.Session()
+        self.sess.run(tf.global_variables_initializer())
 
-    def train(self, n_iterations=10000):
+    def train(self, n_iterations=10000, autoencoder=False, classifier=False, exp_name='exp'):
         mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
-        self.sess.run(tf.global_variables_initializer())
+        loss = 0
+        var_list = []
+        if autoencoder:
+            loss += self.reconstruction_loss
+            var_list += tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='autoencoder')
+
+        if classifier:
+            loss += self.classification_loss
+            var_list += tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='classifier')
+
+        opt = tf.train.AdamOptimizer(1e-4)
+        train_step = opt.minimize(loss, var_list = var_list)
+
+        #opt_vars = [v for v in tf.global_variables() if 'beta' in v.name]
+        #tf.initialize_variables(opt_vars)
+
+        uninit_vars = self.sess.run(tf.report_uninitialized_variables())
+        print uninit_vars
+
+        # init
+        for gb in tf.global_variables():
+            for ui in uninit_vars:
+                if ui in gb.name:
+                    print ui, gb
+                    self.sess.run(gb.initializer)
+
+
         iter_step = n_iterations / 100
         for iteration in range(n_iterations):
             if iteration % iter_step == 0:
@@ -68,9 +88,12 @@ class AutoEncoder:
                 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
                 print '\tOverall accuracy', accuracy.eval(feed_dict = {self.input: mnist.test.images, self.label: mnist.test.labels}, session=self.sess)
 
+                self.render_examples(fn='ae_ex_%s_%08d.png' % (exp_name, iteration))
+                self.render_latent(fn='ae_latent_%s_%08d.png' % exp_name, iteration))
+
             batch = mnist.train.next_batch(100)
             fd = {self.input:batch[0], self.label:batch[1]}
-            self.train_step.run(feed_dict = fd, session=self.sess)
+            train_step.run(feed_dict = fd, session=self.sess)
 
         correct_prediction = tf.equal(tf.argmax(self.label, 1), tf.argmax(self.pred_label, 1))
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
@@ -125,20 +148,8 @@ class AutoEncoder:
         print latent.shape
 
         plt.figure()
-        plt.scatter(latent[:, 0], latent[:, 1], c=test_labels)
+        plt.scatter(latent[:, 0], latent[:, 1], c=test_labels, s=2)
         plt.title('Latent Representation')
         plt.grid(True)
 
         plt.savefig(fn)
-
-n_iterations = 100000
-
-ae = AutoEncoder(use_classification_loss=False)
-ae.train(n_iterations)
-ae.render_examples(fn='autoencoder_examples.png')
-ae.render_latent(fn='autoencoder_latent.png')
-
-ae = AutoEncoder(use_classification_loss=True)
-ae.train(n_iterations)
-ae.render_examples(fn='autoencoder_classification_examples.png')
-ae.render_latent(fn='autoencoder_classification_latent.png')
---
+++ python/auto_encoder/mnist/classifier.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import matplotlib.pyplot as plt
+import numpy as np
+
+class Classifier:
