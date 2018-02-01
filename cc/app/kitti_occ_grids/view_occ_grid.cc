43e6d6186dbe98c7f4ccb3256b1341524561f749
Arash Ushani <aushani@gmail.com>
2018-02-01 13:14:31 -0500

    hacky viewer for bin og

--- cc/app/kitti_occ_grids/view_occ_grid.cc
+++ cc/app/kitti_occ_grids/view_occ_grid.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <osg/ArgumentParser>
 
 #include "library/osg_nodes/occ_grid.h"
@@ -9,6 +10,42 @@
 namespace rt = library::ray_tracing;
 namespace osgn = library::osg_nodes;
 namespace vw = library::viewer;
+
+
+// XXX Hack
+rt::OccGrid LoadBin(const char* fn) {
+  std::ifstream file(fn, std::ios::in | std::ios::binary);
+
+  int kXYRangePixels_ = 16*3;                // +- 4.8 * 3 m
+  int kZRangePixels_ = 16;                   // +- 4.8 m
+  float res = 0.3;
+
+  std::vector<rt::Location> locs;
+  std::vector<float> log_odds;
+
+  for (int i=-kXYRangePixels_; i<=kXYRangePixels_; i++) {
+    for (int j=-kXYRangePixels_; j<=kXYRangePixels_; j++) {
+      for (int k=-kZRangePixels_; k<=kZRangePixels_; k++) {
+        float p = -1.0;
+
+        // gross
+        file.read((char*)(&p), sizeof(float));
+
+        if (p > 0.51 || p < 0.49) {
+          printf("p: %5.3f\n", p);
+          float lo = p > 0.5 ? 1:-1;
+
+          locs.emplace_back(i, j, k);
+          log_odds.push_back(lo);
+        }
+      }
+    }
+  }
+
+  file.close();
+
+  return rt::OccGrid(locs, log_odds, res);
+}
 
 int main(int argc, char** argv) {
   osg::ArgumentParser args(&argc, argv);
@@ -26,6 +63,7 @@ int main(int argc, char** argv) {
 
   au->addCommandLineOption("--og <dirname>", "Occ Grid Filename", "");
   au->addCommandLineOption("--fog <dirname>", "Occ Grid Filename", "");
+  au->addCommandLineOption("--bin <dirname>", "Occ Grid Bin Filename", "");
 
   // handle help text
   // call AFTER init viewer so key bindings have been set
@@ -39,6 +77,13 @@ int main(int argc, char** argv) {
   std::string fn;
   if (args.read("--og", fn)) {
     rt::OccGrid og = rt::OccGrid::Load(fn.c_str());
+
+    vw::Viewer v(&args);
+    osg::ref_ptr<osgn::OccGrid> ogn = new osgn::OccGrid(og);
+    v.AddChild(ogn);
+    v.Start();
+  } else if (args.read("--bin", fn)) {
+    rt::OccGrid og = LoadBin(fn.c_str());
 
     vw::Viewer v(&args);
     osg::ref_ptr<osgn::OccGrid> ogn = new osgn::OccGrid(og);
